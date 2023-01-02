33f3df87a85fa8397fee288b252bd5c5b0c2b900
alaa <AlaaEl-DinAhmed>
2023-01-02 22:01:34 +0200

    Implement module federation for container and products

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,11 +1,18 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
 
-export default {
+module.exports = {
   mode: "development",
   devServer: {
     port: 8080,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:8081/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
